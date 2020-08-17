8b9ec045cc3f87ec95f962fadb3577d2efb388b3
Simmer999 <fearlessleader5900@gmail.com>
2020-08-17 10:27:56 -0600

    Add read module and slideUp function.

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 function slideOutLeft() {
     var writeContainer = document.getElementById("main-book-container");
     writeContainer.classList.toggle("main-boo");
+    var readContainer = document.getElementById("George");
+    readContainer.classList.toggle("main-rea");
 }
 
 
