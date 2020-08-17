633b90f88916960a1c7bae98319730d5838a229e
Simmer999 <fearlessleader5900@gmail.com>
2020-08-17 09:29:09 -0600

    Add slideOutLeft feature.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function save(){
-    var text_to_save=document.getElementById('ch_1-sec_1a').value;
-    localStorage.setItem("text", text_to_save); // save the item
-    }
+
+
+function slideOutLeft() {
+    var writeContainer = document.getElementById("main-book-container");
+    writeContainer.classList.toggle("main-boo");
+}
+
+
